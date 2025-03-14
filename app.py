7e8e2485599b517fb0386cd319436607ec4db5dc
Pham Quoc An <An24@users.noreply.huggingface.co>
2025-03-14 01:33:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ async def root():
     return {"message": "Toxicity Detector API is running on Hugging Face Spaces"}
 
 # Chạy app nếu script được thực thi trực tiếp
-#if __name__ == "__main__":
- #   uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
