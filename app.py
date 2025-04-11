3f8d42e8e7f18cec7130bad11bfc19152e3192fe
anpha@DESKTOP-IT4F327 <anpha@DESKTOP-IT4F327.email@example.com>
2025-04-11 23:29:38 +0700

    Remove LFS-tracked file

--- app.py
+++ app.py
@@ -515,7 +515,8 @@ from typing import List, Dict, Any, Optional, Tuple
 import numpy as np
 import re
 
-tokenizer = AutoTokenizer.from_pretrained("path/to/tokenizer")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
 
 # Thiết lập logging
 logging.basicConfig(
